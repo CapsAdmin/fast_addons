707629ac34643336d8bdbf8a9db8ae1103c74ba6
CapsAdmin <eliashogstvedt@gmail.com>
2017-01-24 23:27:52 +0100

    use Get/SetNWBool instead of Get/SetNetData which is metastruct specific

--- lua/fast_addons/dance2.lua
+++ lua/fast_addons/dance2.lua
@@ -1,22 +1,22 @@
 local function is_dancing(ply)
-	return ply:GetNetData("dancing")
+	return ply:GetNWBool("dancing")
 end
-	
+
 if CLIENT then
 	local bpm = CreateClientConVar("dance_bpm", 120, true, true)
-		
+
 	hook.Add("ShouldDrawLocalPlayer", "dance", function(ply)
 		if is_dancing(ply) then
 			return true
 		end
 	end)
-	
+
 	hook.Add("CalcView", "dance", function(ply, pos)
 		if not is_dancing(ply) then return end
-		
+
 		local pos = pos + ply:GetAimVector() * -100
 		local ang = (ply:EyePos() - pos):Angle()
-		
+
 		return {
 			origin = pos,
 			angles = ang,
@@ -26,7 +26,7 @@ if CLIENT then
 	local beats = {}
 	local suppress = false
 	local last
-	
+
 	hook.Add("CreateMove", "dance", function(cmd)
 		if is_dancing(LocalPlayer()) then
 			if cmd:KeyDown(IN_JUMP) then
@@ -35,19 +35,19 @@ if CLIENT then
 					last = last or time
 					table.insert(beats, time - last)
 					last = time
-					
+
 					local temp = 0
 					for k,v in pairs(beats) do temp = temp + v end
 					temp = temp / #beats
 					temp = 1 / temp
-					
+
 					if #beats > 5 then
 						table.remove(beats, 1)
 					end
-					
+
 					RunConsoleCommand("dance_bpm", (temp * 60))
 					RunConsoleCommand("dance_setrate", bpm:GetInt())
-						
+
 					suppress = true
 				end
 			else
@@ -59,18 +59,18 @@ if CLIENT then
 
 	hook.Add("CalcMainActivity", "dance", function(ply)
 		if is_dancing(ply) then
-			local bpm = (ply:GetNetData("dance_bpm") or 120) / 94
+			local bpm = (ply:GetNWBool("dance_bpm") or 120) / 94
 			local time = (RealTime() / 10) * bpm
 			time = time%2
 			if time > 1 then
 				time = -time + 2
 			end
-			
+
 			time = time * 0.8
 			time = time + 0.11
-			
+
 			ply:SetCycle(time)
-		
+
 			return 0, ply:LookupSequence("taunt_dance")
 		end
 	end)
@@ -78,22 +78,22 @@ end
 
 if SERVER then
 	concommand.Add("dance_setrate", function(ply, _, args)
-		ply:SetNetData("dance_bpm", tonumber(args[1]))
+		ply:SetNWBool("dance_bpm", tonumber(args[1]))
 	end)
-	
+
 	local function addcmd()
 		aowl.AddCommand("dance2", function(ply)
-			if not ply:GetNetData("dancing") then
+			if not ply:GetNWBool("dancing") then
 				aowl.Message(ply, "Dance mode enabled!")
 				aowl.Message(ply, "Tap space to the beat!")
-				ply:SetNetData("dancing", true)
+				ply:SetNWBool("dancing", true)
 			else
 				aowl.Message(ply, "Dance mode disabled.")
-				ply:SetNetData("dancing", false)
+				ply:SetNWBool("dancing", false)
 			end
 		end)
 	end
-	
+
 	if aowl then
 		addcmd()
 	else
@@ -102,9 +102,9 @@ if SERVER then
 			hook.Remove("AowlInitialized", "dance2")
 		end)
 	end
-	
+
 	hook.Add("PlayerDeath", "DancingDeath", function(ply)
-		if ply:GetNetData("dancing") then
+		if ply:GetNWBool("dancing") then
 			ply:ConCommand("aowl dance2")
 		end
 	end)
